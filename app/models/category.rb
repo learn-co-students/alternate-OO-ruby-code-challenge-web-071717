a2ee1e7ffd521c517cf7b264a3fa6a4cb5c2bbca
Zali Perlow <zali.perlow@flatironschool.com>
2017-07-28 14:45:24 -0400

    Done.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,34 @@
 class Category
+
   attr_accessor :name
-  
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.find_or_create_by_name(name)
+    result = self.all.find { |category_obj| category_obj.name == name}
+    if result.nil?
+      Category.new(name)
+      else
+        result
+      end
+  end
+
+  def self.all
+    @@all
+  end
+
+  def articles
+    ArticleCategory.all.select { |art_cat_obj| binding.pry art_cat_obj.category == self }#.map { |art_cat_filtered| art_cat_filtered.article }
+  end
+
+  def category
+    # insert
+  end
+
 
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,21 @@
 require 'bundler/setup'
 Bundler.require
 require_all 'app'
+require 'pry'
+
+bob = Contributor.new('Bob', 'Smith')
+linda = Contributor.new('Linda', 'Jones')
+terry = Contributor.new('Terry', 'Smith')
+
+news = Category.new('news')
+sport = Category.new('sport')
+weather = Category.new('weather')
+
+article1 = Article.new('today', bob, 'todays news', 'news.com', '2.00', news)
+article2 = Article.new('people running', linda, 'todays sports', 'sports.com', '4.00', sport)
+article3 = Article.new('sunny', linda, 'todays weather', 'weather.com', '6.00', weather)
+article4 = Article.new('tomorrow', bob, 'tomorrows news', 'news.com', '8.00', news)
+
+
+
+Pry.start
